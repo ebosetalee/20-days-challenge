ec39b326c0c82fa2b99858c0e23c3549d7ff8d72
ebosetalee <princess01ella.miley@gmail.com>
2020-07-15 16:42:00 +0100

    solution to 2 Convert a number to base n

--- convert-a-number-to-base-n.py
+++ convert-a-number-to-base-n.py
@@ -2,6 +2,7 @@ import random
 number = int(random.random()*1000000000)
 my_list = [2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16]
 base_number = int(random.choice(my_list)) 
+print(base_number)
 
 def calculate_base():
     res=""
@@ -10,7 +11,26 @@ def calculate_base():
       c = a % base_number
       d = a // base_number
       res+=str(c)
-      # print(c)
       a = d
     print("{0} to base {1:2} is {2}".format(number,base_number,res[::-1]))
-calculate_base()
+
+def calclulate_base_16():
+    print("{0} to base 16 is {0:>04x}".format(number))
+
+def base_11_to_15():
+    base_value = "0123456789abcde"
+    res=""
+    a = number
+    while (a > 0):
+      c = a % base_number
+      d = a // base_number
+      res+= base_value[c]
+      a = d
+    print("{0} to base {1} is {2}".format(number,base_number,res[::-1]))
+
+if base_number < 10:
+    calculate_base()
+elif (base_number == 16):
+    calclulate_base_16()
+else:
+    base_11_to_15()
