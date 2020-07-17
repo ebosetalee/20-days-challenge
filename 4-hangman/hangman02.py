4a880b608eba3ed1609301b1dabfcb85233b4a67
ebosetalee <princess01ella.miley@gmail.com>
2020-07-17 19:58:26 +0100

    Resolves conversations

--- 4-hangman/hangman02.py
+++ 4-hangman/hangman02.py
@@ -1,12 +1,13 @@
 import random
 my_list = ["I", "eat", "chicken", "and", "turkey"]
-random_word = str(random.choice(my_list))
-# print(random_word.upper())
+random_word = str(random.choice(my_list).upper())
+print(random_word)
 
-word = input("Kindly type one letter only: ")
+word = input("Kindly type one letter only: ").upper()
 index = 0
-while index < len(random_word):
-    if word in random_word[index]:
+# print(word)
+for i in random_word:
+    if word == i:
         print("YES in {}".format(index))
     index += 1
 if word not in random_word:
