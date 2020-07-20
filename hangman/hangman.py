9e1c0685386168e938da15f766bf4dbd9fd5c9c6
ebosetalee <princess01ella.miley@gmail.com>
2020-07-20 19:10:05 +0100

    Solution to 5b-Hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,15 +3,25 @@ my_list = ["I", "eat", "chicken", "and", "turkey"]
 random_word = str(random.choice(my_list).upper())
 print(random_word)
 
+dic_list = {}
+for letters in random_word:
+    dic_list[letters] = False
+print(dic_list)
+
 word = ""
-while word != random_word and word != "!":
+count = 0
+while word != dic_list and word != "!":
     word = input("Kindly type one letter only: ").upper()
     index = 0
-    for i in random_word:
+    for i in dic_list:
         if word == i:
-            print("YES in {}".format(index))
+            dic_list[word] = True
+            print("YES in {0} and it only took you {1} tries".format(index, count))
         index += 1
+    count += 1
     if word == "!":
+        for val in dic_list:
+            print(val + ": {} ".format(dic_list[val]))
         break
-    elif word not in random_word:
+    elif word not in dic_list:
         print("NO! try again. Type ! to quit")
