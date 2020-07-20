17e540faf44eebfcdf5afbd78bbf0919410c85a2
ebosetalee <princess01ella.miley@gmail.com>
2020-07-20 14:40:15 +0100

    solution to 05-Hangman-03

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,12 +3,17 @@ my_list = ["I", "eat", "chicken", "and", "turkey"]
 random_word = str(random.choice(my_list).upper())
 print(random_word)
 
-word = input("Kindly type one letter only: ").upper()
-index = 0
-# print(word)
-for i in random_word:
-    if word == i:
-        print("YES in {}".format(index))
-    index += 1
-if word not in random_word:
-    print("NO")
+word = ""
+while word != random_word and word != "!":
+    word = input("Kindly type one letter only: ").upper()
+    index = 0
+    for i in random_word:
+        if word == i:
+            print("YES in {}".format(index))
+        index += 1
+    random_word == i
+    print(random_word)
+    if word == "!":
+        break
+    elif word not in random_word:
+        print("NO! try again. Type ! to quit")
