0c93806619c555b358af2caf2d210e0c4bd70f24
ebosetalee <princess01ella.miley@gmail.com>
2020-08-05 17:50:30 +0100

    fixed

--- hangman/lib/database.py
+++ hangman/lib/database.py
@@ -7,9 +7,9 @@ def create_table():
     """sqlite table created to store guesses"""
     db.execute("CREATE TABLE IF NOT EXISTS guesses(letter CHAR)")
 
-def guesses_table(store_guessed_letter):
+def store_guessed_letter(guess):
     """ adds the guesses into the table created"""
-    db.execute("INSERT INTO guesses VALUES('{}')".format(store_guessed_letter))
+    db.execute("INSERT INTO guesses VALUES('{}')".format(guess))
     db.commit()
 
 def drop_table():
